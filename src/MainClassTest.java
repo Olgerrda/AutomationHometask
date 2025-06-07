d6e02fb22a51425d59ef96fed86916550fa579e6
olgerrda <olgerrda@gmail.com>
2025-06-07 21:03:59 +0400

    Lesson 1 Task 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,12 +2,14 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass{
     @Test
-    public void testGetClassNumber() {
-        int number = this.getClassNumber();
-        if (number > 45) {
-            System.out.println("getClassNumber returns number > 45");
+    public void testGetClassString() {
+        String strFromGetClassString = this.getClassString();
+        String strToSearch = "Hello";
+        boolean contains = strFromGetClassString.toLowerCase().contains(strToSearch.toLowerCase());
+        if (contains){
+            System.out.println("Substring found");
         } else {
-            System.out.println("getClassNumber returns number <= 45");
+            System.out.println("Substring not found");
         }
 
     }
